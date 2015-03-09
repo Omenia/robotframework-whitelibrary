23244ce29b212ebe9d5b6c2565e90dee94cd1c39
IsNoGood <ismo.aro@gmail.com>
2015-03-09 16:47:59 +0200

    added WhiteLibrary.dll file copying.

--- PythonWhiteLibrary/setup.py
+++ PythonWhiteLibrary/setup.py
@@ -1,3 +1,4 @@
+import distutils.sysconfig
 from distutils.core import setup
 setup(name         = 'robotframework-whitelibrary',
       version      = '0.0.1',
@@ -6,5 +7,6 @@ setup(name         = 'robotframework-whitelibrary',
       author_email = 'ismo.aro@omenia.fi',
       url          = 'https://github.com/Omenia/robotframework-whitelibrary',
       package_dir  = {'' : 'src'},
-      py_modules = ['WhiteLibrary']
+      py_modules = ['WhiteLibrary'],
+      data_files = [(distutils.sysconfig.get_python_lib(), ["../WhiteLibrary/bin/WhiteLibrary.dll"])]
       )
