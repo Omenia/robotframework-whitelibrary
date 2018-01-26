329546dd3d3988e3d3d4e33dbdb88a6ab7b1eb46
Eeva <eeter@users.noreply.github.com>
2018-01-26 12:52:37 +0200

    Implement search strategy selection by locator prefix

--- WhiteLibrary/WhiteFramework.cs
+++ WhiteLibrary/WhiteFramework.cs
@@ -11,12 +11,20 @@ using TestStack.White.UIItems.ListBoxItems;
 using TestStack.White.UIItems.MenuItems;
 using TestStack.White.Configuration;
 using Castle.Core.Logging;
+using System.Reflection;
 
 namespace WhiteLibrary
 {
     public class WhiteFW
     {
         protected Window window;
+
+        private Dictionary<string, string> strategies = new Dictionary<string, string>
+        {
+            {"id", "ByAutomationId"},
+            {"text", "ByText"},
+            {"index", "AndByIndex"},
+        };
 
         protected void set_logging(string level)
         {
@@ -36,74 +44,106 @@ namespace WhiteLibrary
 
         protected TextBox getTextBox(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return (TextBox)window.Get(searchCriteria);
+            return getItemByLocator<TextBox>(locator);
         }
 
         protected Slider getSlider(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return (Slider)window.Get(searchCriteria);
+            return getItemByLocator<Slider>(locator);
         }
 
         protected Label getLabel(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return window.Get<Label>(searchCriteria);
+            return getItemByLocator<Label>(locator);
         }
-        
+
         protected ComboBox getComboBox(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return window.Get<ComboBox>(searchCriteria);
+            return getItemByLocator<ComboBox>(locator);
         }
-        
+
         protected Button getButton(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return window.Get<Button>(searchCriteria);
+            return getItemByLocator<Button>(locator);
         }
-      
+
         protected RadioButton getRadioButton(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return window.Get<RadioButton>(searchCriteria);
+            return getItemByLocator<RadioButton>(locator);
         }
 
         protected CheckBox getCheckBox(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return window.Get<CheckBox>(searchCriteria);
+            return getItemByLocator<CheckBox>(locator);
         }
 
         protected GroupBox getGroupBox(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return window.Get<GroupBox>(searchCriteria);
+            return getItemByLocator<GroupBox>(locator);
         }
 
         protected Menu getMenu(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByText(locator);
-            return window.Get<Menu>(searchCriteria);
+            return getItemByLocator<Menu>(locator);
         }
 
         protected ListBox getListBox(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByText(locator);
-            return window.Get<ListBox>(searchCriteria);
+            return getItemByLocator<ListBox>(locator);
         }
-        
+
         protected ListItem getListItem(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByText(locator);
-            return window.Get<ListItem>(searchCriteria);
+            return getItemByLocator<ListItem>(locator);
         }
 
         protected ProgressBar getProgressBar(string locator)
         {
-            SearchCriteria searchCriteria = SearchCriteria.ByAutomationId(locator);
-            return (ProgressBar)window.Get(searchCriteria);
+            return getItemByLocator<ProgressBar>(locator);
+        }
+
+        private T getItemByLocator<T>(string locator) where T : IUIItem
+        {
+            var locatorParts = getLocatorParts(locator);
+            string searchStrategy = locatorParts[0];
+            string locatorValue = locatorParts[1];
+
+            if (searchStrategy == "partial_text")
+            {
+                return getItemByPartialText<T>(locatorValue);
+            }
+
+            return getItemBySearchCriteria<T>(searchStrategy, locatorValue);
+        }
+
+        private string[] getLocatorParts(string locator)
+        {
+            if (!locator.Contains("="))
+            {
+                // use automation id as default search strategy if no strategy is defined
+                locator = "id=" + locator;
+            }
+            return locator.Split('=');
+        }
+
+        private T getItemByPartialText<T>(string partialText) where T : IUIItem
+        {
+            IUIItem[] items = window.GetMultiple(SearchCriteria.All);
+            return (T)items.First(x => x.GetType() == typeof(T) && x.Name.Contains(partialText));
+        }
+
+        private T getItemBySearchCriteria<T>(string searchStrategy, string locatorValue) where T : IUIItem
+        {
+            SearchCriteria searchCriteria = getSearchCriteria(searchStrategy, locatorValue);
+            return window.Get<T>(searchCriteria);
+        }
+        private SearchCriteria getSearchCriteria(string searchStrategy, string locatorValue)
+        {
+            string methodName;
+            strategies.TryGetValue(searchStrategy, out methodName);
+            MethodInfo searchMethod = typeof(SearchCriteria).GetMethod(methodName);
+            var methodParameters = new string[] { locatorValue };
+            return (SearchCriteria)searchMethod.Invoke(null, methodParameters);
         }
     }
 }
