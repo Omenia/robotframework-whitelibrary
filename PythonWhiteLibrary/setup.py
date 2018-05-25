c3bf696c5adcf21c5d7369f1080c99d60d7c0ae4
Ismo Aro <ismo.aro@gmail.com>
2018-05-25 13:48:55 +0300

    added pypi release

--- PythonWhiteLibrary/setup.py
+++ PythonWhiteLibrary/setup.py
@@ -3,10 +3,9 @@ from distutils.core import setup
 setup(name         = 'robotframework-whitelibrary',
       version      = '0.0.1',
       description  = 'Windows GUI testing library for Robot Framework',
-      author       = 'Omenia Ltd',
-      author_email = 'ismo.aro@omenia.fi',
+      author       = 'SALabs',
       url          = 'https://github.com/Omenia/robotframework-whitelibrary',
       package_dir  = {'' : 'src'},
       py_modules = ['WhiteLibrary'],
-      data_files = [(distutils.sysconfig.get_python_lib(), ["../WhiteLibrary/bin/WhiteLibrary.dll"])]
+      package_data = {'robotframework-whitelibrary': ["../WhiteLibrary/bin/WhiteLibrary.dll"]}
       )
