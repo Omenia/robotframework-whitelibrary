aeb49e6ab2c9a344c42d208d4759b382b3c14033
Juha Alhonen <juha.alhonen@siili.com>
2018-08-08 09:48:45 +0300

    Adding needed artifacts

--- PythonWhiteLibrary/setup.py
+++ PythonWhiteLibrary/setup.py
@@ -1,9 +1,10 @@
 import distutils.sysconfig
 from distutils.core import setup
 setup(name         = 'robotframework-whitelibrary',
-      version      = '0.0.1',
+      version      = '0.0.2',
       description  = 'Windows GUI testing library for Robot Framework',
       author       = 'SALabs',
+      author       = 'to.be.added@noexist89a887.org',
       url          = 'https://github.com/Omenia/robotframework-whitelibrary',
       package_dir  = {'' : 'src'},
       py_modules   = ['WhiteLibrary'],
