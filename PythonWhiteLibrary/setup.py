ec3b99a0a609b11059e5ab9fe1bc07d4c4d7ddf8
Juha Alhonen <juha.alhonen@siili.com>
2018-08-09 15:15:39 +0300

    Test autodeployment

--- PythonWhiteLibrary/setup.py
+++ PythonWhiteLibrary/setup.py
@@ -2,7 +2,7 @@ import distutils.sysconfig
 from distutils.core import setup
 from distutils.extension import Extension
 setup(name         = 'robotframework-whitelibrary2',
-      version      = '0.0.4',
+      version      = '0.0.5',
       description  = 'Windows GUI testing library for Robot Framework',
       author       = 'SALabs',
       author_email = 'to.be.added@noexist89a887.org',
