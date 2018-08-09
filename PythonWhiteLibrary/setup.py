faaa3a8fecf889a03b1017874cf9e1df73b0411b
Juha Alhonen <juha.alhonen@siili.com>
2018-08-09 09:37:19 +0300

    Increasing version number

--- PythonWhiteLibrary/setup.py
+++ PythonWhiteLibrary/setup.py
@@ -2,7 +2,7 @@ import distutils.sysconfig
 from distutils.core import setup
 from distutils.extension import Extension
 setup(name         = 'robotframework-whitelibrary2',
-      version      = '0.0.2',
+      version      = '0.0.3',
       description  = 'Windows GUI testing library for Robot Framework',
       author       = 'SALabs',
       author_email = 'to.be.added@noexist89a887.org',
