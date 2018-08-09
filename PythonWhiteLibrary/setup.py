763a3e79c111571555b5552c9b2335a90364985a
Juha Alhonen <juha.alhonen@siili.com>
2018-08-09 11:35:13 +0300

    Trying to add dll to package

--- PythonWhiteLibrary/setup.py
+++ PythonWhiteLibrary/setup.py
@@ -2,11 +2,12 @@ import distutils.sysconfig
 from distutils.core import setup
 from distutils.extension import Extension
 setup(name         = 'robotframework-whitelibrary2',
-      version      = '0.0.3',
+      version      = '0.0.4',
       description  = 'Windows GUI testing library for Robot Framework',
       author       = 'SALabs',
       author_email = 'to.be.added@noexist89a887.org',
       url          = 'https://github.com/Omenia/robotframework-whitelibrary',
       packages     = ['WhiteLibrary'],
-      package_dir  = {'WhiteLibrary' : ''}
+      package_dir  = {'WhiteLibrary' : ''},
+      package_data = {'WhiteLibrary' : 'CSWhiteLibrary.dll'},
       )
