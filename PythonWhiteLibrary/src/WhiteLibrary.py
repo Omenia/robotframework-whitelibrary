d3004bc9d5f1b3027a5977b996bfe201684363c4
Ismo Aro <ismo.aro@siili.com>
2018-09-05 17:48:00 +0300

    fixed print command

--- PythonWhiteLibrary/src/WhiteLibrary.py
+++ PythonWhiteLibrary/src/WhiteLibrary.py
@@ -6,7 +6,7 @@ class WhiteLibrary(object):
     WHITE_LIB=None
 
     def __init__(self, dev=False):
-        print '{}'.format(dev)
+        print '%r' % dev
         if dev:
             clr.AddReference('CSWhiteLibrary')
         else:
