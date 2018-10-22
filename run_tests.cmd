b01e076610c064bd22455d16c73442df46069ccd
Jani Mikkonen <jani.mikkonen@siili.com>
2018-10-22 20:28:44 +0300

    Try to guess all the paths if deployment isnt available

--- run_tests.cmd
+++ run_tests.cmd
@@ -1,2 +1,10 @@
 @echo on
-robot --outputdir output --noncritical unstable -P %DEPLOYMENT% %* --loglevel DEBUG:INFO PythonWhiteLibrary/atests
+if "%DEPLOYMENT%" == "" (
+  if "%NUGET%" == "" (
+    set NUGET=packages
+  )
+  set PP=-P PythonWhiteLibrary\src -P PythonWhiteLibrary -P %NUGET%\Castle.Core.3.3.0\lib\net45\ -P %NUGET%\TestStack.White.0.13.3\lib\net40 -P %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40
+) else (
+  set PP=-P %DEPLOYMENT%
+)
+robot --outputdir output --noncritical unstable %PP% %* --loglevel DEBUG:INFO PythonWhiteLibrary/atests
