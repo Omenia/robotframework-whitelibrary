7264f1fb2903dc68f372949cd2967046f75c4926
Jenna Kaisanlahti <jenna.kaisanlahti@siili.com>
2018-11-16 17:24:24 +0200

    temp commit with click fix

--- PythonWhiteLibrary/src/WhiteLibrary.py
+++ PythonWhiteLibrary/src/WhiteLibrary.py
@@ -9,7 +9,7 @@ from TestStack.White.UIItems.WindowItems import Window
 from TestStack.White.UIItems import Button, TextBox, Label, RadioButton, Slider, CheckBox, ProgressBar, UIItem
 from TestStack.White.UIItems.TabItems import Tab
 from TestStack.White.UIItems.TreeItems import Tree
-from TestStack.White.UIItems.ListBoxItems import ComboBox
+from TestStack.White.UIItems.ListBoxItems import ComboBox, ListBox
 from TestStack.White.UIItems.MenuItems import Menu
 from TestStack.White.UIItems.Finders import SearchCriteria
 from TestStack.White.InputDevices import Keyboard
@@ -86,8 +86,18 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | text | inserted string | M |
         """
-        textBox = self._get_item_by_locator(TextBox, locator)
+        textBox = self._get_typed_item_by_locator(TextBox, locator)
         textBox.Text = text
+
+    def click_item(self, locator):
+        """
+        Click an item
+        Parameters
+        ----------
+        locator - element id, text or index prefixed with <locator_type>=
+        """
+        item = self._get_item_by_locator(locator)
+        item.Click()
 
     def right_click_item(self, locator):
         """
@@ -96,7 +106,7 @@ class WhiteLibrary(object):
         ----------
         locator - element id, text or index prefixed with <locator_type>=
         """
-        item = self._get_item_by_locator(UIItem, locator)
+        item = self._get_item_by_locator(locator)
         item.RightClick()
 
     def double_click_item(self, locator):
@@ -106,7 +116,7 @@ class WhiteLibrary(object):
         ----------
         locator - element id, text or index prefixed with <locator_type>=
         """
-        item = self._get_item_by_locator(UIItem, locator)
+        item = self._get_item_by_locator(locator)
         item.DoubleClick()
 
     def press_special_key(self, key):
@@ -142,7 +152,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | value | inserted value (must be between scale) | M |
         """
-        slider = self._get_item_by_locator(Slider, locator)
+        slider = self._get_typed_item_by_locator(Slider, locator)
         slider.Value = float(value)
 
     def verify_slider_value(self, locator, expected):
@@ -152,7 +162,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected double | M |
         """
-        slider = self._get_item_by_locator(Slider, locator)
+        slider = self._get_typed_item_by_locator(Slider, locator)
         self._verify_value(float(expected), slider.Value)
 
     def verify_progressbar_value(self, locator, expected):
@@ -162,7 +172,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | expected | expected value | M |
         """
-        progressbar = self._get_item_by_locator(ProgressBar, locator)
+        progressbar = self._get_typed_item_by_locator(ProgressBar, locator)
         self._verify_value(float(expected), progressbar.Value)
 
     def verify_text_in_textbox(self, locator, expected):
@@ -172,7 +182,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected text | M |
         """
-        textbox = self._get_item_by_locator(TextBox, locator)
+        textbox = self._get_typed_item_by_locator(TextBox, locator)
         self._verify_value(expected, textbox.Text)
 
     def verify_label(self, locator, expected):
@@ -182,7 +192,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected text | M |
         """
-        label = self._get_item_by_locator(Label, locator)
+        label = self._get_typed_item_by_locator(Label, locator)
         self._verify_value(expected, label.Text)
 
     def select_combobox_value(self, locator, value):
@@ -192,7 +202,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | value | selected value | M |
         """
-        combobox = self._get_item_by_locator(ComboBox, locator)
+        combobox = self._get_typed_item_by_locator(ComboBox, locator)
         combobox.Select(value)
 
     def select_listbox_value(self, locator, value):
@@ -202,7 +212,21 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | value | selected value | M |
         """
-        self.WHITE_LIB.select_listbox_value(locator, value)
+        listbox = self._get_typed_item_by_locator(ListBox, locator)
+        listbox.Select(value)
+
+    def listbox_selection_should_be(self, locator, value):
+        """
+        Check that the value on the listbox is selected
+        Parameters
+        ----------
+        locator - element id, text or index prefixed with <locator_type>=
+        value - value that should be selected
+        """
+        listbox = self._get_typed_item_by_locator(ListBox, locator)
+        if listbox.SelectedItemText != value:
+            raise AssertionError("Expected listbox selection to be " +
+                                 value + ", was " + listbox.SelectedItemText)
 
     def toggle_check_box(self, locator):
         """
@@ -210,7 +234,7 @@ class WhiteLibrary(object):
         | Arguments | Usage | (M)andatory / (O)ptional |
         | locator | element id | M |
         """
-        checkbox = self._get_item_by_locator(CheckBox, locator)
+        checkbox = self._get_typed_item_by_locator(CheckBox, locator)
         checkbox.Toggle()
 
     def select_combobox_index(self, locator, index):
@@ -220,7 +244,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | index | combobox index | M |
         """
-        combobox = self._get_item_by_locator(ComboBox, locator)
+        combobox = self._get_typed_item_by_locator(ComboBox, locator)
         combobox.Select(int(index))
 
     def verify_combobox_item(self, locator, expected):
@@ -240,7 +264,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected value | M |
         """
-        combobox = self._get_item_by_locator(ComboBox, locator)
+        combobox = self._get_typed_item_by_locator(ComboBox, locator)
         self._verify_value(expected, combobox.EditableText)
 
     def verify_button(self, locator, expected):
@@ -250,7 +274,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected text | M |
         """
-        button = self._get_item_by_locator(Button, locator)
+        button = self._get_typed_item_by_locator(Button, locator)
         self._verify_value(expected, button.Text)
 
     def click_button(self, locator):
@@ -259,10 +283,18 @@ class WhiteLibrary(object):
         | Arguments | Usage | (M)andatory / (O)ptional |
         | locator | element id | M |
         """
-        button = self._get_item_by_locator(Button, locator)
+        button = self._get_typed_item_by_locator(Button, locator)
         button.Click()
 
-    def _get_item_by_locator(self, item_type, locator):
+    def _get_typed_item_by_locator(self, item_type, locator):
+        search_criteria = self._get_search_criteria(locator)
+        return self.window.Get[item_type](search_criteria)
+
+    def _get_item_by_locator(self, locator):
+        search_criteria = self._get_search_criteria(locator)
+        return self.window.Get(search_criteria)
+
+    def _get_search_criteria(self, locator):
         if "=" not in locator:
             locator = "id=" + locator
         search_strategy, locator_value = locator.split("=")
@@ -270,8 +302,7 @@ class WhiteLibrary(object):
             locator_value = int(locator_value)
         search_method = STRATEGIES[search_strategy]
         method = getattr(SearchCriteria, search_method)
-        search_criteria = method(locator_value)
-        return self.window.Get[item_type](search_criteria)
+        return method(locator_value)
 
     def verify_radio_button(self, locator, expected):
         """
@@ -280,7 +311,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected value | M |
         """
-        radiobutton = self._get_item_by_locator(RadioButton, locator)
+        radiobutton = self._get_typed_item_by_locator(RadioButton, locator)
         self._verify_value(bool(expected), radiobutton.IsSelected)
 
     def verify_check_box(self, locator, expected):
@@ -290,7 +321,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | actual | expected value | M |
         """
-        checkbox = self._get_item_by_locator(CheckBox, locator)
+        checkbox = self._get_typed_item_by_locator(CheckBox, locator)
         self._verify_value(bool(expected), checkbox.IsSelected)
 
     def select_radio_button(self, locator):
@@ -299,7 +330,7 @@ class WhiteLibrary(object):
         | Arguments | Usage | (M)andatory / (O)ptional |
         | locator | element id | M |
         """
-        radiobutton = self._get_item_by_locator(RadioButton, locator)
+        radiobutton = self._get_typed_item_by_locator(RadioButton, locator)
         radiobutton.Select()
 
     def verify_menu(self, locator, expected):
@@ -309,7 +340,7 @@ class WhiteLibrary(object):
         | locator | element id | M |
         | expected | expected text in value | M |
         """
-        menu = self._get_item_by_locator(Menu, locator)
+        menu = self._get_typed_item_by_locator(Menu, locator)
         self._verify_value(expected, menu.Name)
 
     def click_menu_button(self, locator):
@@ -318,7 +349,7 @@ class WhiteLibrary(object):
         | Arguments | Usage | (M)andatory / (O)ptional |
         | locator | element id | M |
         """
-        menu_button = self._get_item_by_locator(Menu, locator)
+        menu_button = self._get_typed_item_by_locator(Menu, locator)
         menu_button.Click()
 
     def select_modal_window(self, locator):
@@ -331,27 +362,27 @@ class WhiteLibrary(object):
 
     def select_tab_page(self, locator, title):
         """ Selects a tab page. """
-        tab = self._get_item_by_locator(Tab, locator)
+        tab = self._get_typed_item_by_locator(Tab, locator)
         tab.SelectTabPage(title)
 
     def select_tree_node(self, locator, *node_path):
         """ Selects a tree node. """
-        tree = self._get_item_by_locator(Tree, locator)
+        tree = self._get_typed_item_by_locator(Tree, locator)
         tree.Nodes.GetItem(node_path).Select()
 
     def expand_tree_node(self, locator, *node_path):
         """ Expands a tree node. """
-        tree = self._get_item_by_locator(Tree, locator)
+        tree = self._get_typed_item_by_locator(Tree, locator)
         tree.Nodes.GetItem(node_path).Expand()
 
     def double_click_tree_node(self, locator, *node_path):
         """ Double-clicks a tree node. """
-        tree = self._get_item_by_locator(Tree, locator)
+        tree = self._get_typed_item_by_locator(Tree, locator)
         tree.Nodes.GetItem(node_path).DoubleClick()
 
     def right_click_tree_node(self, locator, *node_path):
         """ Right-clicks a tree node. """
-        tree = self._get_item_by_locator(Tree, locator)
+        tree = self._get_typed_item_by_locator(Tree, locator)
         tree.Nodes.GetItem(node_path).RightClick()
 
     def take_desktop_screenshot(self):
