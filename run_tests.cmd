aaab59207941170b38f72b6d60a6b3cd0277a5b8
Jenna Kaisanlahti <jenna.kaisanlahti@siili.com>
2018-11-27 17:37:32 +0200

    Try to fix run script and deploy paths

--- run_tests.cmd
+++ run_tests.cmd
@@ -5,6 +5,6 @@ if "%NUGET%" == "" (
 if "%DEPLOYMENT%" == "" (
   set PP=-P %NUGET%\Castle.Core.3.3.0\lib\net45\ -P %NUGET%\TestStack.White.0.13.3\lib\net40 -P %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40 -P PythonWhiteLibrary\src\keywords -P PythonWhiteLibrary\src\keywords\items -P PythonWhiteLibrary\src -P PythonWhiteLibrary
 ) else (
-  set PP=-P %DEPLOYMENT%
+  set PP=-P %DEPLOYMENT% -P %DEPLOYMENT%\keywords -P %DEPLOYMENT%\keywords\items
 )
 robot --outputdir output --noncritical unstable %PP% %* --loglevel DEBUG:INFO PythonWhiteLibrary/atests
