b7598f1632e490ac7269c66f4d26e9e5635bbd2b
Jenna Kaisanlahti <jenna.kaisanlahti@siili.com>
2018-11-27 15:21:05 +0200

    Revert "Try to fix deploy paths"
    This reverts commit c5ec18ebfa4f8e23b20e2787cad084a7d7235a6d.

--- deploy.cmd
+++ deploy.cmd
@@ -1,24 +1,5 @@
 mkdir %DEPLOYMENT%
-mkdir %DEPLOYMENT%\keywords
-mkdir %DEPLOYMENT%\keywords\items
 copy PythonWhiteLibrary\src\WhiteLibrary.py %DEPLOYMENT%\
-copy PythonWhiteLibrary\src\keywords\__init__.py %DEPLOYMENT%\keywords\
-copy PythonWhiteLibrary\src\keywords\application.py %DEPLOYMENT%\keywords\
-copy PythonWhiteLibrary\src\keywords\keyboard.py %DEPLOYMENT%\keywords\
-copy PythonWhiteLibrary\src\keywords\librarycomponent.py %DEPLOYMENT%\keywords\
-copy PythonWhiteLibrary\src\keywords\robotlibcore.py %DEPLOYMENT%\keywords\
-copy PythonWhiteLibrary\src\keywords\window.py %DEPLOYMENT%\keywords\
-copy PythonWhiteLibrary\src\keywords\items\__init__.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\buttons.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\label.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\listcontrols.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\menu.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\progressbar.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\slider.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\tab.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\textbox.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\tree.py %DEPLOYMENT%\keywords\items\
-copy PythonWhiteLibrary\src\keywords\items\uiitem.py %DEPLOYMENT%\keywords\items\
 copy PythonWhiteLibrary\version.py %DEPLOYMENT%\
 copy PythonWhiteLibrary\src\__init__.py %DEPLOYMENT%\
 copy PythonWhiteLibrary\setup.py %DEPLOYMENT%\
