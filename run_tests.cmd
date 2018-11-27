f8a92e597a4f212fa7c178a9878c5a6586d9078b
Jenna Kaisanlahti <jenna.kaisanlahti@siili.com>
2018-11-27 17:15:48 +0200

    Try to fix run script

--- run_tests.cmd
+++ run_tests.cmd
@@ -1,9 +1,9 @@
 @echo on
+if "%NUGET%" == "" (
+  set NUGET=packages
+)
 if "%DEPLOYMENT%" == "" (
-  if "%NUGET%" == "" (
-    set NUGET=packages
-  )
-  set PP=-P PythonWhiteLibrary\src -P PythonWhiteLibrary -P %NUGET%\Castle.Core.3.3.0\lib\net45\ -P %NUGET%\TestStack.White.0.13.3\lib\net40 -P %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40
+  set PP=-P %NUGET%\Castle.Core.3.3.0\lib\net45\ -P %NUGET%\TestStack.White.0.13.3\lib\net40 -P %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40 -P PythonWhiteLibrary\src\keywords -P PythonWhiteLibrary\src\keywords\items -P PythonWhiteLibrary\src -P PythonWhiteLibrary
 ) else (
   set PP=-P %DEPLOYMENT%
 )
