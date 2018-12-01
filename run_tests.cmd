6b7b52b692ff2d684b9be33bdabad32c0f03f399
eeter <eeter@users.noreply.github.com>
2018-12-01 10:26:58 +0200

    Copy dlls to library package in a script step in yaml, and remove pythonpath settings from run_tests

--- run_tests.cmd
+++ run_tests.cmd
@@ -1,12 +1,5 @@
 @echo on
-if "%NUGET%" == "" (
-  set NUGET=packages
-)
-if "%DEPLOYMENT%" == "" (
-  set PP=-P %NUGET%\Castle.Core.3.3.0\lib\net45\ -P %NUGET%\TestStack.White.0.13.3\lib\net40 -P %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40 -P src\WhiteLibrary\keywords -P src\WhiteLibrary\keywords\items -P src
-) else (
-  set PP=-P %DEPLOYMENT% -P %DEPLOYMENT%\keywords -P %DEPLOYMENT%\keywords\items
-)
+
 pushd %~dp0
-robot --outputdir output --noncritical unstable %PP% %* --loglevel DEBUG:INFO atests
+robot --outputdir output --noncritical unstable %* --loglevel DEBUG:INFO atests
 popd
