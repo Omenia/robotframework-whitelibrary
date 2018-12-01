7969f0cbd9d37d43a9ac8b62f710e5ba7ae7b1c5
eeter <eeter@users.noreply.github.com>
2018-12-01 10:46:09 +0200

    Restore vsts deployment step and modify deploy script to copy dlls under the python package

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,9 @@
 mkdir %DEPLOYMENT%
+mkdir src\WhiteLibrary\bin\ 
 
-copy %NUGET%\Castle.Core.3.3.0\lib\net45\Castle.Core.dll src\WhiteLibrary\bin
-copy %NUGET%\TestStack.White.0.13.3\lib\net40\TestStack.White.dll src\WhiteLibrary\bin
-copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.Reporting.dll src\WhiteLibrary\bin
-copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.ScreenObjects.dll src\WhiteLibrary\bin
-
-xcopy /s src\WhiteLibrary %DEPLOYMENT% 
+copy %NUGET%\Castle.Core.3.3.0\lib\net45\Castle.Core.dll src\WhiteLibrary\bin\
+copy %NUGET%\TestStack.White.0.13.3\lib\net40\TestStack.White.dll src\WhiteLibrary\bin\
+copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.Reporting.dll src\WhiteLibrary\bin\
+copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.ScreenObjects.dll src\WhiteLibrary\bin\
 
 copy build.info %DEPLOYMENT%\
