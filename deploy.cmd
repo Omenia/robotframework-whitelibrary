16281d5d1e761bf68b35e52b41ac153e90501906
eeter <eeter@users.noreply.github.com>
2018-12-01 09:26:38 +0200

    Add library install step to vsts yaml and modify deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,32 +1,10 @@
 mkdir %DEPLOYMENT%
-mkdir %DEPLOYMENT%\keywords
-mkdir %DEPLOYMENT%\keywords\items
 
-copy src\WhiteLibrary\version.py %DEPLOYMENT%\
-copy src\WhiteLibrary\__init__.py %DEPLOYMENT%\
-copy setup.py %DEPLOYMENT%\
-copy src\WhiteLibrary\keywords\__init__.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\application.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\keyboard.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\librarycomponent.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\robotlibcore.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\window.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\screenshot.py %DEPLOYMENT%\keywords\
-copy src\WhiteLibrary\keywords\items\__init__.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\buttons.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\label.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\listcontrols.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\menu.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\progressbar.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\slider.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\tab.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\textbox.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\tree.py %DEPLOYMENT%\keywords\items\
-copy src\WhiteLibrary\keywords\items\uiitem.py %DEPLOYMENT%\keywords\items\
+copy %NUGET%\Castle.Core.3.3.0\lib\net45\Castle.Core.dll src\WhiteLibrary\bin
+copy %NUGET%\TestStack.White.0.13.3\lib\net40\TestStack.White.dll src\WhiteLibrary\bin
+copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.Reporting.dll src\WhiteLibrary\bin
+copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.ScreenObjects.dll src\WhiteLibrary\bin
 
-copy %NUGET%\Castle.Core.3.3.0\lib\net45\Castle.Core.dll %DEPLOYMENT%\
-copy %NUGET%\TestStack.White.0.13.3\lib\net40\TestStack.White.dll %DEPLOYMENT%\
-copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.Reporting.dll %DEPLOYMENT%\
-copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.ScreenObjects.dll %DEPLOYMENT%\
+xcopy /s src\WhiteLibrary %DEPLOYMENT% 
 
 copy build.info %DEPLOYMENT%\
