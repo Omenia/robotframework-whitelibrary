0b1e5c3ca71ddc980094e1c51565df2677f7020f
Ismo Aro <ismo.aro@gmail.com>
2018-12-13 10:14:41 +0200

    src direcotry to deploy

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,5 @@
 mkdir %DEPLOYMENT%
+mkdir %DEPLOYMENT%\src\
 mkdir src\WhiteLibrary\bin\ 
 
 copy %NUGET%\Castle.Core.3.3.0\lib\net45\Castle.Core.dll src\WhiteLibrary\bin\
@@ -6,6 +7,6 @@ copy %NUGET%\TestStack.White.0.13.3\lib\net40\TestStack.White.dll src\WhiteLibra
 copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.Reporting.dll src\WhiteLibrary\bin\
 copy %NUGET%\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.ScreenObjects.dll src\WhiteLibrary\bin\
 
-xcopy src %DEPLOYMENT%\ /s /a
+xcopy src %DEPLOYMENT%\src\ /s /a
 copy setup.py %DEPLOYMENT%\
 copy build.info %DEPLOYMENT%\
