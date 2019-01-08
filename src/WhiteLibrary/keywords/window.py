6643733efd5089f585d6031f83d0d54463ac3cf5
eeter <eeter@users.noreply.github.com>
2019-01-08 14:17:16 +0200

    Remove unnecessary console logging

--- src/WhiteLibrary/keywords/window.py
+++ src/WhiteLibrary/keywords/window.py
@@ -7,20 +7,17 @@ from robot.api import logger
 class WindowKeywords(LibraryComponent):
     @keyword
     def attach_window(self, window_title):
-        """
-        Attach WhiteLibrary to a window of already attached application.
+        """Attach WhiteLibrary to a window of already attached application.
         See `Attach Application By Id', `Attach Application By Name´ or
         `Launch application` for more details.
 
         ``window_title`` is the title of the window.
         """
         self.state.window = self.state.app.GetWindow(window_title)
-        logger.console("window {}".format(self.state.window))
 
     @keyword
     def select_modal_window(self, window_title):
-        """
-        Select modal window.
+        """Select modal window.
 
         ``window_title`` is the title of the window.
         """
