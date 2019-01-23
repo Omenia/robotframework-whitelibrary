82448a35c967e49ab6f15a639caf0aa78023de87
Eero Mäki-Esko <eero.maki-esko@siili.com>
2019-01-23 13:56:09 +0200

    Added delayed element needed NuGet packages
    Microsoft.Bcl
    Microsoft.Bcl.Async
    Microsoft.Bcl.Build

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,12 @@
 mkdir %DEPLOYMENT%
 mkdir %DEPLOYMENT%\src\
-mkdir src\WhiteLibrary\bin\ 
+mkdir src\WhiteLibrary\bin\
 
 copy %NUGET%\Castle.Core.3.3.0\lib\net45\Castle.Core.dll src\WhiteLibrary\bin\
 copy %NUGET%\TestStack.White.0.13.3\lib\net40\TestStack.White.dll src\WhiteLibrary\bin\
+copy %NUGET%\Microsoft.Bcl.Async.1.0.168\lib\net40\Microsoft.Threading.Tasks.dll src\WhiteLibrary\bin\
+copy %NUGET%\Microsoft.Bcl.Async.1.0.168\lib\net40\Microsoft.Threading.Tasks.Extensions.dll src\WhiteLibrary\bin\
+copy %NUGET%\Microsoft.Bcl.Async.1.0.168\lib\net40\Microsoft.Threading.Tasks.Extensions.Desktop.dll src\WhiteLibrary\bin\
 
 rmdir docs /s /q
 
