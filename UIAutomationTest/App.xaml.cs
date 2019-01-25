75628900939fe2fa41459248d1e03a5c62435795
Eero Mäki-Esko <eero.maki-esko@siili.com>
2019-01-25 07:37:55 +0200

    Launch application keyword accepts command line arguments.
    SUT does not yet use the arguments.

--- UIAutomationTest/App.xaml.cs
+++ UIAutomationTest/App.xaml.cs
@@ -10,7 +10,15 @@ namespace UIAutomationTest
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
+    private var listOfStrings = new List<string>();
+
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 1)
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+        }
     }
 }
