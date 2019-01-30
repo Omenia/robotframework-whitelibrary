1e0dfb5848eebd0c3708a4435764b9666bd6de7d
Eeva <eeter@users.noreply.github.com>
2019-01-30 13:22:32 +0200

    Add keyword click_popup_menu_item

--- src/WhiteLibrary/keywords/items/menu.py
+++ src/WhiteLibrary/keywords/items/menu.py
@@ -6,8 +6,7 @@ from WhiteLibrary.keywords.robotlibcore import keyword
 class MenuKeywords(LibraryComponent):
     @keyword
     def verify_menu(self, locator, expected):
-        """
-        Verifies the text of a menu item.
+        """Verifies the text of a menu item.
 
         ``locator`` is the locator of the menu item.
 
@@ -18,10 +17,22 @@ class MenuKeywords(LibraryComponent):
 
     @keyword
     def click_menu_button(self, locator):
-        """
-        Clicks a menu button.
+        """Clicks a menu button.
 
         ``locator`` is the locator of the menu button.
         """
         menu_button = self.state._get_typed_item_by_locator(Menu, locator)
         menu_button.Click()
+
+    @keyword
+    def click_popup_menu_item(self, *text_path):
+        """Clicks an item in the currently open popup/context menu.
+
+        ``text_path`` is the path to the item to click.
+
+        Examples:
+        | Click Popup Menu Item | Paste | | # click an item in the root level |
+        | Click Popup Menu Item | Refactor | Rename | # click a sub item |
+        """
+        popup_menu = self.state.window.Popup
+        popup_menu.Item(list(text_path)).Click()
