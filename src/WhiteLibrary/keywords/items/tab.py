38a837df2329b698f5f2bb393c0035023ca34a6c
Eero Mäki-Esko <eero.maki-esko@siili.com>
2019-03-06 10:50:33 +0200

    Improved Tab handling
    New keywords:
    select_tab_page_with_index
    get_tab_pages

--- src/WhiteLibrary/keywords/items/tab.py
+++ src/WhiteLibrary/keywords/items/tab.py
@@ -14,5 +14,33 @@ class TabKeywords(LibraryComponent):
 
         ``title`` is the title of the tab page.
         """
-        tab = self.state._get_typed_item_by_locator(Tab, locator)
-        tab.SelectTabPage(title)
+        tab_control = self.state._get_typed_item_by_locator(Tab, locator)
+        tab_control.SelectTabPage(title)
+
+    @keyword
+    def select_tab_page_with_index(self, locator, index):
+        """
+        Selects a tab page.
+
+        ``locator`` is the locator of the tab control item or Tab item object.
+        Locator syntax is explained in `Item locators`.
+
+        ``index`` is integer index the tab page. Indexing starts from 0.
+        """
+        tab_control = self.state._get_typed_item_by_locator(Tab, locator)
+        tab_control.SelectTabPage(int(index))
+
+    @keyword
+    def get_tab_pages(self, locator):
+        """
+        Gets all tab pages and returns them as a list.
+        The resulting list contains TabPage objects. You can select tab using TabPage as follows:
+        @{tabs}=    Get Tab Pages    myTabControl
+        Select Tab Page    tabControl    ${tabs[0].Name}
+
+        ``locator`` is the locator of the tab control item or Tab item object.
+        Locator syntax is explained in `Item locators`.
+
+        """
+        tab_control = self.state._get_typed_item_by_locator(Tab, locator)
+        return tab_control.Pages
