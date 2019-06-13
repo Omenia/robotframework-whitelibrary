6e239d603392a52ba3bd71efbf35fada9baefad1
Eeva <eeter@users.noreply.github.com>
2019-06-13 13:33:26 +0300

    Refactor

--- src/WhiteLibrary/keywords/items/__init__.py
+++ src/WhiteLibrary/keywords/items/__init__.py
@@ -3,7 +3,6 @@ from .label import LabelKeywords  # noqa: F401 #pylint: disable=unused-import
 from .listcontrols import ListKeywords  # noqa: F401 #pylint: disable=unused-import
 from .listview import ListViewKeywords  # noqa: F401 #pylint: disable=unused-import
 from .menu import MenuKeywords  # noqa: F401 #pylint: disable=unused-import
-from .mouse import MouseKeywords  # noqa: F401 #pylint: disable=unused-import
 from .progressbar import ProgressbarKeywords  # noqa: F401 #pylint: disable=unused-import
 from .slider import SliderKeywords  # noqa: F401 #pylint: disable=unused-import
 from .tab import TabKeywords  # noqa: F401 #pylint: disable=unused-import
