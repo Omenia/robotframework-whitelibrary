813c5d7817b604a268d622877afeffa3a543f0bf
Jani Mikkonen <jani.mikkonen@siili.com>
2019-07-01 14:02:25 +0300

    Use Salabs.TestStack.White

--- local_install.cmd
+++ local_install.cmd
@@ -8,9 +8,9 @@ REM copy all dll packages into same location
 mkdir src\WhiteLibrary\bin\
 
 copy packages\Castle.Core.3.3.0\lib\net45\Castle.Core.dll src\WhiteLibrary\bin\
-copy packages\TestStack.White.0.13.3\lib\net40\TestStack.White.dll src\WhiteLibrary\bin\
-copy packages\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.Reporting.dll src\WhiteLibrary\bin\
-copy packages\TestStack.White.ScreenObjects.0.13.3\lib\net40\TestStack.White.ScreenObjects.dll src\WhiteLibrary\bin\
+copy packages\Salabs.TestStack.White.0.13.4\lib\net40\TestStack.White.dll src\WhiteLibrary\bin\
+copy packages\Salabs.TestStack.White.ScreenObjects.0.13.4\lib\net40\TestStack.White.Reporting.dll src\WhiteLibrary\bin\
+copy packages\Salabs.TestStack.White.ScreenObjects.0.13.4\lib\net40\TestStack.White.ScreenObjects.dll src\WhiteLibrary\bin\
 
 REM remove files from possible previous installation
 if exist "files.txt" (
